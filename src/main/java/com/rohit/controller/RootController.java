cf71620da1fd4a45896f2dab86baa1cbed244411
Rohuu <rohuu99022@gmail.com>
2023-04-10 23:04:30 +0530

    simple variable expression

--- src/main/java/com/rohit/controller/RootController.java
+++ src/main/java/com/rohit/controller/RootController.java
@@ -1,5 +1,6 @@
 package com.rohit.controller;
 import com.rohit.model.Employee;
+import com.rohit.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,18 @@ public class RootController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("pageTitle", "Thymeleaf");
-        model.addAttribute("userName", "Mohit");
-        model.addAttribute("userId", 555);
+//        model.addAttribute("pageTitle", "Thymeleaf");
+//        model.addAttribute("userName", "Mohit");
+//        model.addAttribute("userId", 555);
         model.addAttribute("scientists", List.of("Albert Einstein",
                 "Niels Bohr",
                 "James Clerk Maxwell"));
+
+        Person person=new Person();
+        person.setName("Rohit");
+        person.setAge(25);
+
+        model.addAttribute("person1",person);
 
         return "index"; // returning index.html
     }
